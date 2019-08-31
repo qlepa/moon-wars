23aaef4f4e8faf3060744f925049624a5c293162
Tomasz Klepacki <t.klepacki@gmail.com>
2019-08-31 23:05:55 +0200

    add counter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,12 +9,33 @@ class App extends Component {
   state = {
     errorFetch: null,
     successFetch: false,
-    view: 'Category',
-    cards: []
+    view: "Category",
+    cards: [],
   };
 
+  fetchCategoryCounter = (category) => {
+    return swapi
+      .get(`/${category}`)
+      .then(response => response.data.count)
+      .catch(error => error);
+  }
+
+  //const itemsPerPage = 10
+  //const howManyPages = counter/itemsPerPage
+  //const rest = counter % 10
+  // const page = Math.floor(Math.random() * howManyPages) + 1
+  // const itemIndex = page === howManyPages ? rest : itemsPerPage
+  // const randomItem = Math.floor(Math.random() * itemIndex-1)
+
+  // const response = await swapi.get(`/${category}/&page=${page}`)
+
+  // const item = response.data[itemIndex]
+
+  //Zakładamy, że na stronie jest 10 itemów, sprawdzamy ile jest stron dla wybranej kategorii. ZNajdujemy modulo, które jest liczbą itemów na ostatniej stronie,
+  //losujemy numer strony a zaraz potem losujemy index itemu na tej stronie i go zwracamy.
+
   fetchCards = async category => {
-    const counter = category === "people" ? 87 : 37;
+    const counter = await this.fetchCategoryCounter(category);
     const randomNum = Math.floor(Math.random() * counter) + 1;
     const link = `/${category}/${randomNum}`;
     const response = await swapi.get(`${link}`).catch(error => error);
@@ -24,36 +45,50 @@ class App extends Component {
         errorFetch: response.message
       });
     } else {
+      //TODO clear array when runs again
+      console.log({counter})
       this.setState(state => ({
-        cards: [...state.cards, response.data],
-        successFetch: true,
+        cards: [...state.cards, response.data]
+        // successFetch: true,
       }));
+      if (this.state.cards.length === 2) {
+        this.setState({
+          successFetch: true
+        });
+      }
     }
   };
 
-  renderView = (view) => {
+  renderView = view => {
     this.setState({
-      view: view,
-    })
+      view: view
+    });
   };
 
   render() {
-    console.log(this.state.cards)
+    // console.log(this.state.cards);
     switch (this.state.view) {
-    case 'BattleGround':
-      return (
-        <Wrapper>
-          <BattleGround fetchCards={this.fetchCards} cards={this.state.cards} />
-        </Wrapper>
-      );
-    default:
-      return (
-        <Wrapper>
-          <Category fetchCards={this.fetchCards} renderView={this.renderView} successFetch={this.state.successFetch} />
-        </Wrapper>
-      );
+      case "BattleGround":
+        return (
+          <Wrapper>
+            <BattleGround
+              fetchCards={this.fetchCards}
+              cards={this.state.cards}
+            />
+          </Wrapper>
+        );
+      default:
+        return (
+          <Wrapper>
+            <Category
+              fetchCards={this.fetchCards}
+              renderView={this.renderView}
+              successFetch={this.state.successFetch}
+            />
+          </Wrapper>
+        );
+    }
   }
-}
 }
 
 export default App;
