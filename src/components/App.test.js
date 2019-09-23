d57d8b9220f46fd4c0ba303d76b13a16134c4e4e
Tomasz Klepacki <tomasz.klepacki@iforbet.pl>
2019-09-23 16:03:24 +0200

    wip

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,14 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { fetchCards } from "./App";
+import moxios from "moxios";
+import swapi from "../api/swapi";
+import { fetchCategoryCounter } from "./App";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
 });
 
-it("axios call", () => {
-  const data = fetchCards("people");
-  console.log(data)
-})
+describe("Check Axios", () => {
+  beforeEach(() => {
+    moxios.install(swapi);
+  });
+
+  afterEach(() => {
+    moxios.uninstall(swapi);
+  });
+
+  // it('fetches the posts of a given user', (done) => {
+  //   const category = "people"
+  //   const expectedPosts = ['Post1', 'Post2']
+
+  //   moxios.stubRequest(`/${category}`, {
+  //     status: 200,
+  //     response: expectedPosts
+  //   })
+
+  //   const onFulfilled = sinon.spy()
+  //   fetchCategoryCounter("people").then(onFulfilled)
+
+  //   moxios.wait(() => {
+  //     expect(onFulfilled.getCall(0).args[0].data).toBe(expectedPosts)
+  //     done()
+  //   })
+  // })
+  it("test get", async () => {
+    const expectedPosts = 87;
+    const pytam = fetchCategoryCounter("people")
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({ status: 200, response: expectedPosts }); //mocked response
+    });
+
+    const result = await pytam();
+    console.log(result); 
+    expect(result).toEqual(expectedPosts);
+  });
+});
